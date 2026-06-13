0be277b9157ad0be1d9bdab028d9af5feb23b5db
adityanavale-tech <adityaanavale@gmail.com>
2026-06-13 18:24:37 -0400

    Class 1 Homework(Organizing Telemetry in Telem.java)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,4 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import org.firstinspires.ftc.teamcode.Telem;
 public class Robot {
+
+    public Telem telem;
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telem.java
@@ -1,4 +1,58 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.pedropathing.follower.Follower;
+import com.qualcomm.*;
+
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import com.qualcomm.*;
 public class Telem {
+
+    public Telemetry telemetry;
+    public Follower follower;
+
+    DcMotorEx motor = hardwareMap.get(DcMotorEx.class, "motor");
+    double rpm = Math.abs(motor.getVelocity()*2.14);
+
+
+
+    public void Outtake(){
+        telemetry.addData("RRM: ", rpm);
+        telemetry.addData("Motor Power: ", motor.getPower());
+        telemetry.addData("Current: ",motor.getCurrent(CurrentUnit.AMPS));
+        telemetry.update();
+    }
+    public void Pose(){
+        telemetry.addData("Pose (x,y): ", follower.getPose().getAsFTCStandardCoordinates());
+        telemetry.addData("Robot Velocity: ",follower.getVelocity().getMagnitude());
+        telemetry.addData("Heading: ",follower.getTotalHeading());
+
+        telemetry.update();
+
+    }
+    public static void TeleHelper(Gamepad gamepad, Telemetry telemetry, double rpm, Follower follower){
+        if(gamepad1.rightTriggerWasPressed()){
+            telemetry.addLine("Right Trigger Pressed");
+            telemetry.addData("RPM is at: ", rpm);
+
+            telemetry.update();
+        }
+        else {
+            telemetry.addLine("Outtake at Rest");
+        }
+        if(gamepad.aWasPressed()){
+            telemetry.addData("The robot is at: ", follower.getPose().getAsFTCStandardCoordinates());
+        }
+    }
+
+
+
 }
