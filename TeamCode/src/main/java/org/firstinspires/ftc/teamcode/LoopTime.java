a10c63ca5fef8133c4d2c961475eba64ef0e4ac6
adityanavale-tech <adityaanavale@gmail.com>
2026-06-14 09:54:59 -0400

    pedro pathing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LoopTime.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class LoopTime extends LinearOpMode {
+
+    private ElapsedTime actionTimer;
+
+    @Override
+    public void runOpMode(){
+
+
+
+
+
+        waitForStart();
+        while (opModeIsActive()){
+
+        }
+
+    }
+}
